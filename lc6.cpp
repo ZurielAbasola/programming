5de01e549cbad9045b91dde5c3c62a5a132c6e8f
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-05-08 02:40:31 -0600

    Partial solution

---
+++ lc6.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) return s;
+
+        string res = "";
+
+        for (int i = 0; i < numRows; i++) {
+            int increment = 2 * (numRows - 1);
+
+            for (int j = 0; j)
+        }
+
+
+        return res;
+    }
+};
