79617c2356695017d334d182ef6f4f713dad6c34
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-05-10 01:45:15 -0600

    Solution to Leetcode Problem #6

--- lc6.cpp
+++ lc6.cpp
@@ -8,13 +8,17 @@ public:
         if (numRows == 1) return s;
 
         string res = "";
-
-        for (int i = 0; i < numRows; i++) {
+        for (int r = 0; r < numRows; r++) {
             int increment = 2 * (numRows - 1);
 
-            for (int j = 0; j)
-        }
+            for (int i = r; i < s.length(); i += increment) {
+                res += s[i];
 
+                if (r > 0 && r < (numRows - 1) && (i + increment - 2 * r) < s.length()) {
+                    res += s[i + increment - 2 * r];
+                }
+            }
+        }
 
         return res;
     }
