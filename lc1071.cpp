8cb44e47c2aed0262722e34f4c97fd797f45cdeb
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-05-10 17:14:19 -0600

    Solution to Leetcode problem 1071

---
+++ lc1071.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int gcd(int a, int b) 
+{ 
+    int result = min(a, b); 
+    while (result > 0) { 
+        if (a % result == 0 && b % result == 0) { 
+            break; 
+        } 
+        result--; 
+    } 
+  
+    return result; 
+} 
+
+    string gcdOfStrings(string str1, string str2) {
+        string res;
+
+        if (str1 + str2 != str2 + str1) {
+            return "";
+        }   
+
+        int gcdLength = gcd(str1.size(), str2.size());
+
+        return str1.substr(0, gcdLength);
+    }
+};
+
+int main() {
+    Solution sol;
+    string str1, str2;
+
+    cin >> str1 >> str2;
+
+    cout << sol.gcdOfStrings(str1, str2);
+
+    return 0;
+}
