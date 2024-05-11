be42f25331f2269a06ead7c4f736c2613099a038
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-05-11 08:41:12 -0600

    Leetcode problem 1431 solution

--- lc1071.cpp
+++ lc1071.cpp
@@ -5,15 +5,14 @@ using namespace std;
 
 class Solution {
 public:
-    int gcd(int a, int b) 
-{ 
+    int gcd(int a, int b) { 
     int result = min(a, b); 
     while (result > 0) { 
         if (a % result == 0 && b % result == 0) { 
             break; 
         } 
         result--; 
-    } 
+    }; 
   
     return result; 
 } 
---
+++ lc1431.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<bool> kidsWithCandies (vector<int>& candies, int extraCandies) {
+		vector<bool> result;
+		int max = *max_element(candies.begin(), candies.end());
+
+		for (auto itr = candies.cbegin(); itr != candies.end(); ++itr) {
+			if (*itr + extraCandies >= max) {
+				result.push_back(true);
+			}
+
+			result.push_back(false);
+		}
+
+		return result;
+	}
+};
