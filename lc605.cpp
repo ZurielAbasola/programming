c376ac9f9da644342fe17fe590337e1f8bed6251
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-05-13 02:41:55 -0600

    Solution to Leetcode problem 605

--- lc605.cpp
+++ lc605.cpp
@@ -6,8 +6,22 @@ using namespace std;
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-    	if(n == 0) {
-    		return true;
-    	}
+    	if (n == 0) {
+            return true;
+        }
+
+        int count = 0;
+        auto size = flowerbed.size();
+
+        for (size_t i = 0; i < size; ++i) {
+            if (flowerbed[i] == 0 &&
+                (i == 0 || flowerbed[i - 1] == 0) &&
+                (i == size - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1; 
+                ++count;
+            }
+        }
+
+        return count >= n;
     }
 };
