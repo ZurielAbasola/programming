226b3d1a5823bdfbfa2f3b2803433e1d7c2cbd0b
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-05-15 14:49:56 -0600

    Make Leetcode directory

---
+++ Leetcode/lc283.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+
+        for (int step = 0; step < n; ++step) {
+            if (nums[step] != 0) continue;
+
+            for (int i = step; i < n - 1; ++i) {
+                int temp = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = temp;
+            }
+        }
+    }
+};
