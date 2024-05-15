f319a4431c2ba506be4d31c56738f8d92c7460cc
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-05-14 18:33:27 -0600

    Solution to Leetcode Problem #151

--- lc151.cpp
+++ lc151.cpp
@@ -6,10 +6,35 @@ using namespace std;
 class Solution {
 public:
     string static reverseWords(string s) {
+        if (s.empty()) return s;
+
         stack<string> stack;
         string result;
+        for (int i = 0; i < s.length(); ++i) {
+            string word;
+            if (s[i] == ' ') continue;
+            while (i < s.length() && s[i] != ' ') {
+                word += s[i++];
+            }
+            stack.push(word);
+        }
 
+        while(!stack.empty()) {
+            result += stack.top();
+            stack.pop();
+            if (!stack.empty()) result += ' ';
+        }
 
         return result;
     }
 };
+
+int main() {
+    string s;
+
+    getline(cin, s);
+
+    cout << Solution::reverseWords(s);
+
+    return 0;
+}
