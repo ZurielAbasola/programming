ae5e588d1922fc4cc28b67e6b53964744a4c9a08
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-05-16 10:46:19 -0600

    Started Leetcode problem 11

---
+++ Leetcode/lc11.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int area = 0;
+
+        int length = height.size();
+        for (int i = 0; i < height.size(); ++i) {
+            for (int j = 0; j < )
+        }
+        
+        return area;
+    }
+};
