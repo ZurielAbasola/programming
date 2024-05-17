2623f2cd6580835d251138e03bbf87b316fded3e
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-05-16 23:31:19 -0600

    Two pointer solution to Problem 11

--- Leetcode/lc11.cpp
+++ Leetcode/lc11.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 class Solution {
 public:
     int maxArea(vector<int>& height) {
         int area = 0;
 
-        int length = height.size();
-        for (int i = 0; i < height.size(); ++i) {
-            for (int j = 0; j < )
+        int left = 0;
+        int right = height.size() - 1;
+        while (left < right) {
+            int length = right - left;
+            area = area > (min(height[left], height[right]) * length) ? area
+                    : (min(height[left], height[right]) * length);
+
+            if (height[left] <= height[right]) ++left;
+            else --right;
         }
-        
+
         return area;
     }
 };
