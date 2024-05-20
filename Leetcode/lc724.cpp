746ea54922d7dfdc1c52346eb8b9552df0551ce3
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-05-20 16:17:16 -0600

    Prefix sum solution

---
+++ Leetcode/lc724.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            total += nums[i];
+        }
+
+        if (total - nums[0] == 0) return 0;
+
+        int l_sum = 0;
+        for (int i = 1; i < nums.size(); ++i) {
+            l_sum += nums[i - 1];
+            int r_sum = total - l_sum - nums[i];
+
+            if (l_sum == r_sum) return i;
+        }
+
+        return -1;
+    }
+};
