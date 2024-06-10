b10f879fe60ad42cb66b24a74bc86a9ee64f2b3f
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-06-09 19:50:30 -0600

    Stack and que solution

---
+++ Leetcode/lc2390.cpp
@@ -0,0 +1,22 @@
+#include <string>
+
+// TODO: Finish solution for this
+using namespace std;
+
+class Solution {
+public:
+  string removeStars(string s) {
+    string res = "";
+    
+    for (int i = 0; i < s.size(); ++i) {
+      if (s[i] == '*') {
+        res.pop_back();
+        continue;
+      }
+      res.push_back(s[i]);
+    }
+
+    return res;
+  }
+};
+ 
