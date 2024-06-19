166d459f927846d93180575602bffa5d5ad77459
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-06-19 11:43:31 -0600

    Comments added

--- Leetcode/lc700.cpp
+++ Leetcode/lc700.cpp
@@ -1,14 +1,14 @@
-/*
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// TODO: Finish problem solution -> Learn Binary Search
+
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
