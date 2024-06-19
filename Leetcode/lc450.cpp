f4e9ab26f201afcd26da3fb1f96265456e4da722
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-06-19 16:21:48 -0600

    New problem

---
+++ Leetcode/lc450.cpp
@@ -0,0 +1,18 @@
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// TODO: Find solution
+ 
+class Solution {
+public:
+  TreeNode* deleteNode(TreeNode* root, int key) {
+
+  }
+};
