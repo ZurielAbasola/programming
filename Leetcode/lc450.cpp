4c159ba126c1ab451dfc9ea1d2a7a283c3cd7115
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-06-24 22:55:38 -0600

    Solution

--- Leetcode/lc450.cpp
+++ Leetcode/lc450.cpp
@@ -8,11 +8,33 @@ struct TreeNode {
   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-// TODO: Find solution
+// TODO: Review solution to problem 
  
 class Solution {
 public:
   TreeNode* deleteNode(TreeNode* root, int key) {
+    if (root == nullptr) 
+        return root;
 
+    if (key > root->val)
+        root->right = deleteNode(root->right, key);
+    else if (key < root->val) 
+        root->left = deleteNode(root->left, key);
+    else {
+      if (root->left == nullptr)
+          return root->right;
+      else if (root->right == nullptr)
+          return root->left;
+      
+
+      TreeNode* curr = root->right;
+      while (curr->left != nullptr) {
+        curr = curr->left;
+      }
+      root->val = curr->val;
+      root->right = deleteNode(root->right, root->val);
+    }
+    
+    return root;
   }
 };
